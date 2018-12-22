266509e19a57401fbe9e5f297adc3e24e15d6f57
niusmallnan <niusmallnan@gmail.com>
2018-12-22 16:43:37 +0800

    Add zfs

--- images/02-console/prebuild.sh
+++ images/02-console/prebuild.sh
@@ -13,3 +13,10 @@ DISTRIB_ID=${DISTRIB_ID}
 DISTRIB_RELEASE=${VERSION}
 DISTRIB_DESCRIPTION="${DISTRIB_ID} ${VERSION}"
 EOF
+
+# add zfs
+ZFS_DIR="/tmp/os-zfs/"
+rm -rf ./zfs
+mkdir -p ./zfs
+cp -rf ${ZFS_DIR}/* ./zfs/
+rm -rf ./zfs/usr/lib/modules
